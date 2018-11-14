2afec1f086a8ef82122168f59ad675850af272d1
paswd <paswd@yandex.ru>
2018-11-14 23:09:18 +0300

    Added main methods

--- main.cpp
+++ main.cpp
@@ -15,15 +15,64 @@ public:
 	TNum Y;
 };
 
-TNum f(TNum x) {
+TNum f1(TNum x) {
+	return log(x);
+}
+TNum k1(TNum x, TNum s) {
+	return (4*s - 5*x) / (s * s);
+}
+
+
+TNum df1(TNum x) {
+	return 1./x;
+}
+TNum dk1(TNum x, TNum s) {
+	//return 2 * x * exp(x*x - s*s);
+	return -5. / (s * s);
+}
+
+TNum f1_new(TNum x) {
+	return df1(x) / k1(x,x);
+}
+TNum k1_new(TNum x, TNum s) {
+	return dk1(x, s) / k1(x, x);
+}
+
+TNum f2(TNum x) {
 	return exp(x * x);
 }
-TNum k(TNum x, TNum s) {
+TNum k2(TNum x, TNum s) {
 	return exp(x*x - s*s);
 }
 
-TNum yCorrect(TNum x) {
-	return exp(x*x + x);
+TNum yCorrect(TNum x, size_t mode) {
+	if (mode == 2) {
+		return exp(x*x + x);
+	}
+	return x*x * (log(x) + 1);
+}
+
+void Volterra_I_Solve(TNum a, TNum b, size_t n, TNum &h, vector <Solution> &res) {
+	h = (b - a) / (n - 1);
+
+	res.resize(n);
+	TNum x_tmp = a;
+	for (size_t i = 0; i < res.size(); i++) {
+		res[i].X = x_tmp;
+		x_tmp += h;
+	}
+
+	res[0].Y = f1_new(a);
+
+	for (size_t i = 1; i < n; i++) {
+		TNum s = 0;
+		if (i > 0) {
+			for (size_t j = 0; j < i - 1; j++) {
+				s += k1_new(res[i].X, res[j].X) * res[j].Y;
+			}
+		}
+		res[i].Y = (1 / (1 - h/2 * k1_new(res[i].X, res[i].X))) * (f1_new(res[i].X) + h/2 * k1_new(res[i].X, res[1].X) * res[1].Y + h * s);
+	}
 }
 
 void Volterra_II_Solve(TNum a, TNum b, size_t n, TNum &h, vector <Solution> &res) {
@@ -36,47 +85,50 @@ void Volterra_II_Solve(TNum a, TNum b, size_t n, TNum &h, vector <Solution> &res
 		x_tmp += h;
 	}
 
-	res[0].Y = f(a);
+	res[0].Y = f2(a);
 
 	for (size_t i = 1; i < n; i++) {
 		TNum s = 0;
 		if (i > 0) {
 			for (size_t j = 0; j < i - 1; j++) {
-				s += k(res[i].X, res[j].X) * res[j].Y;
+				s += k2(res[i].X, res[j].X) * res[j].Y;
 			}
 		}
-		res[i].Y = (1 / (1 - h/2 * k(res[i].X, res[i].X))) * (f(res[i].X) + h/2 * k(res[i].X, res[1].X) * res[1].Y + h * s);
+		res[i].Y = (1 / (1 - h/2 * k2(res[i].X, res[i].X))) * (f2(res[i].X) + h/2 * k2(res[i].X, res[1].X) * res[1].Y + h * s);
 	}
-
 }
 
 int main(int argc, char* argv[]) {
-	if (argc < 4) {
+	if (argc < 5) {
 		cout << "# ERROR: Required data is not set" << endl;
 		return 1;
 	}
 	vector <Solution> res;
-	//TNum a, b, h;
-	//size_t n;
-	//cin >> a >> b >> n;
-	TNum a = (TNum) atof(argv[1]);
-	TNum b = (TNum) atof(argv[2]);
-	size_t n = (size_t) atoi(argv[3]);
+	size_t mode = (size_t) atoi(argv[1]);
+	TNum a = (TNum) atof(argv[2]);
+	TNum b = (TNum) atof(argv[3]);
+	size_t n = (size_t) atoi(argv[4]);
 	TNum h;
 
-	Volterra_II_Solve(a, b, n, h, res);
+	if (mode == 1) {
+		Volterra_I_Solve(a, b, n, h, res);
+	} else if (mode == 2) {
+		Volterra_II_Solve(a, b, n, h, res);
+	} else {
+		cout << "# ERROR: Unknown mode" << endl;
+		return 2;
+	}
 
 	ofstream outNumeral("plot-numeral.dat");
 	ofstream outOriginal("plot-original.dat");
 
 	for (size_t i = 0; i < res.size(); i++) {
-		//cout << res[i].X << " " << res[i].Y << " " << yCorrect(res[i].X) << endl;
 		outNumeral << res[i].X << " " << res[i].Y << endl;
 	}
 	outNumeral.close();
 
 	for (TNum curr = a; curr <= b; curr += STEP_ORIG) {
-		outOriginal << curr << " " << yCorrect(curr) << endl;
+		outOriginal << curr << " " << yCorrect(curr, mode) << endl;
 	}
 
 	outOriginal.close();
--- plot.sh
+++ plot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 make > /dev/null
-./nm-kp $1 $2 $3 > plot.dat
+./nm-kp $1 $2 $3 $4 > plot.dat
 gnuplot -persist "plot.gp"
